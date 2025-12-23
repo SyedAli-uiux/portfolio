4295a03ef2b208e66fbea83569960ca41ba02fe5
SyedAli-uiux <syedali1820010@gmail.com>
2025-12-23 20:59:25 +0530

    initial code

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // allows access from other devices
-    port: 5174,       // optional: your port
-  }
-})
+  base: "/portfolio/",   // 🔥 IMPORTANT
+});
